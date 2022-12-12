508bdd0341af2a3fa7f799c2ed9d053b7238881e
Rodrigo Salgado <rodrigosdc20.ma@hotmail.com>
2022-12-12 19:23:58 -0300

    feat: pause menu

---
+++ Piauí Politicos Adventure/Assets/Game/Scripts/Scenes/VolumeController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeController : MonoBehaviour {
+    public AudioMixer audioMixer;
+    public void SetVolume (float volume) {
+        audioMixer.SetFloat("volume", volume);
+    }
+}
